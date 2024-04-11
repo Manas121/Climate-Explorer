5df4e027ff3f9efcc266701aaa186ec997ae88c2
ksuoo <kailesuoo@gmail.com>
2024-04-10 22:54:26 -0600

    fixed bugs

--- src/main/java/com/example/demo/rest/GeolocationList.java
+++ src/main/java/com/example/demo/rest/GeolocationList.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import com.example.demo.GeolocationService;
+import com.example.demo.WeatherService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -14,10 +15,11 @@ public class GeolocationList extends VerticalLayout {
         grid.setItems(service.getGeolocation(location));
         add(grid);
 
-//        double weatherInput[] = new double[1];
-//        Geolocation[] geo = service.getGeolocation(location);
-//        weatherInput[0] = geo[0].getLat();
-//        weatherInput[1] = geo[0].getLon();
+        double weatherInput[] = new double[2];
+        Geolocation[] geo = service.getGeolocation(location);
+        weatherInput[0] = geo[0].getLat();
+        weatherInput[1] = geo[0].getLon();
+        // add something to call the weather api
 
 
 
